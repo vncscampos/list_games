fa50a61d0ab59230c98c62643dc5e5f3c7a0d558
vncscampos <viniolic2018@gmail.com>
2023-07-10 21:26:23 -0300

    tirando axios-cache-adapter

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,13 +1,7 @@
 import axios from 'axios';
-import { setupCache } from 'axios-cache-adapter';
-
-const cache = setupCache({
-    maxAge: 15 * 60 * 1000, // Tempo máximo de cache em milissegundos (aqui definido como 15 minutos)
-  });
 
 const api = axios.create({
     baseURL: 'https://games-test-api-81e9fb0d564a.herokuapp.com/api/',
-    cache: cache
 });
 
 export default api;
